5c6f0cce1048cd0aecb65327f915d51637162de3
Nikita <nikita.ableev@mail.ru>
2022-01-24 17:46:34 +0300

    #First commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import 'Styles/_app.scss';
+
+import './scripts/accordion.js'
+import './scripts/swipers.js'
+import './scripts/fancybox.js'
+import './scripts/header.js'
+import './scripts/parsley.js'
+
+$(() => {
+  require("./scripts/backend.js");
+})
