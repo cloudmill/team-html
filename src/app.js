f6af65e6fd8dfbf37ecd2e42f660e7270b430f35
Nikita <nikita.ableev@mail.ru>
2022-01-31 01:34:31 +0300

    Update

--- src/app.js
+++ src/app.js
@@ -14,21 +14,31 @@ $(() => {
 // loader
 
 $(window).on("load", () => {
-  $("body").css("overflow", "hidden");
+  const body = $('body')
+
+  body.addClass('hidden')
 
   $(".loader").addClass("hidden");
+  const loaderItems = document.querySelectorAll('.loader__item')
+
+  for (let i = 1; i < 6; i++) {
+    setTimeout(() => {
+      loaderItems[i - 1].classList.remove('active')
+      loaderItems[i].classList.add('active')
+    }, 100 * i);
+  }
+
   setTimeout(() => {
-
-    $("body").css("overflow", "");
-
+    body.removeClass('hidden')
+    
     AOS.init({
       once: true, 
       offset: 0,
       duration: 1000,
     });
-
+    
     if (process.env.NODE_ENV === "production") {
       window.scrollTo(0, 0);
     }
-  }, 1500);
+  }, 2000);
 });
